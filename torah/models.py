44d82dc8a12c507fc5319cb715498c2eed6b3fbd
suhailvs <suhailvs@gmail.com>
2018-11-21 08:12:40 +0530

    save paleo word

--- torah/models.py
+++ torah/models.py
@@ -1,6 +1,21 @@
 from django.db import models
 
+class Line(models.Model):
+    TITLES = (
+        ('genesis', 'genesis'),
+        ('exodus', 'exodus'),
+        ('leviticus', 'leviticus'),
+        ('numbers', 'numbers'),
+        ('deuteronomy', 'deuteronomy')
+    )
+    title = models.CharField(max_length = 15,choices = TITLES)
+    chapter = models.IntegerField()
+    line = models.IntegerField()
+
 
 class Word(models.Model):
     name = models.CharField(max_length=200)
     desc = models.TextField()
+    lines = models.ManyToManyField(Line)
+
+
