e2a12d08466ffba4dbc7b1a4d7528ebb9b243ee9
suhailvs <suhailvs@gmail.com>
2018-11-23 07:58:06 +0530

    minor update

--- updatetranslation_of_paleoword.py
+++ updatetranslation_of_paleoword.py
@@ -1,23 +1,31 @@
-
 import json
-
 from torah.models import Word
 
 def wt():
-	paleo = json.loads(open('torah/json/paleo/genesis.json').read())
-	trans = json.loads(open('torah/json/mtt/genesis.json').read())
-	paleo_data = paleo['text']
-	trans_data = trans['text']
+	"""
+	USAGE
+	=====
+	./manage.py shell
+	>>> import updatetranslation_of_paleoword
+	>>> updatetranslation_of_paleoword.wt()
+	"""
+
+	fp_paleo = json.loads(open('torah/json/paleo/genesis.json').read())
+	fp_trans = json.loads(open('torah/json/mtt/genesis.json').read())
+	paleo_data = fp_paleo['text'] # [['line1','line2'...], ...]
+	#trans_data = fp_trans['text'] # [[['word1', ...], ...], ...]
 	
-	for chapter in paleo_data:
-		for i,line in enumerate(chapter):
-			t = trans_data['text'][i][::-1]
-			for j,word in enumerate(line.split(' ')):
+	for i in range(len(paleo_data)):
+		for j in range(len(paleo_data[i])):
+			line = paleo_data[i][j]
+			#print(trans_data[i])
+			trans = fp_trans['text'][i][j][::-1] #[  [  [
+			
+			for n,word in enumerate(line.split(' ')):
 				item = Word.objects.get(name = word)
 				if item.translation:
 					print('There is already a translation')
+					# print(trans[n])
 				else:
-					item.translation = t[j]
+					item.translation = trans[n]
 					item.save()
-
-	
